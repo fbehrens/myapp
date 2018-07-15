41ba5b04e8eb08934e829228d8d5a448aeca4a66
Frank Behrens <fbehrens@gmail.com>
2018-07-15 13:15:18 +0200

    change dockerUser and ImageName

--- build.fsx
+++ build.fsx
@@ -112,8 +112,8 @@ Target.create "Bundle" (fun _ ->
     Shell.copyDir publicDir "src/Client/public" FileFilter.allFiles
 )
 
-let dockerUser = "safe-template"
-let dockerImageName = "safe-template"
+let dockerUser = "fbehrens"
+let dockerImageName = "SafeApp"
 let dockerFullName = sprintf "%s/%s" dockerUser dockerImageName
 
 Target.create "Docker" (fun _ ->
